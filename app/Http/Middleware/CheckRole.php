267942f40f6a0916f9835a2c18075c423a889bd6
Mostafiz Ur Rahman <cdr.mostafiz@gmail.com>
2026-05-21 20:52:20 +0600

    restrict booking creation to admin and branch roles

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (!$user || !$user->roles()->whereIn('name', $roles)->exists()) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,9 @@ Route::middleware('auth')->group(function () {
     Route::resource('transaction-types', TransactionTypeController::class);
     Route::resource('passenger-statuses', PassengerStatusController::class);
     Route::get('/bookings', fn() => view('bookings.index'))->name('bookings.index');
-    Route::resource('bookings', BookingController::class);
+    Route::resource('bookings', BookingController::class)->except(['create', 'store']);
+    Route::get('/bookings/create', [BookingController::class, 'create'])->name('bookings.create')->middleware('role:Super Admin,Co Admin,Branch Manager,Branch Staff');
+    Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store')->middleware('role:Super Admin,Co Admin,Branch Manager,Branch Staff');
     Route::resource('passengers', PassengerController::class);
     Route::patch('/passengers/{passenger}/status', [PassengerController::class, 'updateStatus'])->name('passengers.update-status');
     Route::post('/passengers/{passenger}/documents', [PassengerController::class, 'uploadDocument'])->name('passengers.documents.store');
