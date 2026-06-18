8006254da22ea2e6c568be910e8de19751228c41
shantoriaz6 <22103285@iubat.edu>
2026-06-18 15:50:00 +0600

    visa submit form net visa cost issue solved

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -518,7 +518,7 @@ if ($route) {
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-slate-700 mb-1">Net Visa Cost (SAR)</label>
-                        <input type="number" x-model="visaSubmitForm.netVisaCost" readonly class="w-full px-4 py-2 border border-slate-200 rounded-lg bg-slate-50 text-slate-600">
+                        <input type="number" x-model="visaSubmitForm.netVisaCost" @input="calculateVisaCost()" class="w-full px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-400 focus:border-slate-400 outline-none" placeholder="0">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-slate-700 mb-1">Selling Price (SAR)</label>
