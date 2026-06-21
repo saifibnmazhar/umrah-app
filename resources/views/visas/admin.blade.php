f716116657d5e3f082bff3988085e707fb4a2d79
FUZLUL HAQUE <fazlul5788@gmail.com>
2026-06-21 15:59:04 +0600

    Visa Agents

--- resources/views/visas/admin.blade.php
+++ resources/views/visas/admin.blade.php
@@ -227,7 +227,15 @@
             </div>
         </div>
 
-        <div class="mt-6 flex justify-center">
+        <div class="mt-6 flex justify-center"
+             @click.prevent="
+                 const el = $event.target.closest('a');
+                 if (el && el.href) {
+                     const url = new URL(el.href);
+                     url.searchParams.set('tab', activeTab);
+                     window.location.href = url.toString();
+                 }
+             ">
             {{ $visaAgents->links() }}
         </div>
     </div>
