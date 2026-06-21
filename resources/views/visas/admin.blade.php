5e33f666a28c90170fec17e340a93398219128f3
FUZLUL HAQUE <fazlul5788@gmail.com>
2026-06-21 16:04:36 +0600

    Visa Agent Costs

--- resources/views/visas/admin.blade.php
+++ resources/views/visas/admin.blade.php
@@ -169,9 +169,17 @@
                 </table>
             </div>
 
-            <div class="mt-4 flex justify-center">
-                {{ $visaAgentCosts->links() }}
-            </div>
+        <div class="mt-4 flex justify-center"
+             @click.prevent="
+                 const el = $event.target.closest('a');
+                 if (el && el.href) {
+                     const url = new URL(el.href);
+                     url.searchParams.set('tab', activeTab);
+                     window.location.href = url.toString();
+                 }
+             ">
+            {{ $visaAgentCosts->links() }}
+        </div>
         </div>
     </div>
 
