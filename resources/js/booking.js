9a5b0b5f2a2a5ad4378f922b63e4d5ff6c8c324a
FUZLUL HAQUE <fazlul5788@gmail.com>
2026-06-23 13:03:19 +0600

    currency issue

--- resources/js/booking.js
+++ resources/js/booking.js
@@ -1652,7 +1652,7 @@ Alpine.data('createBookingApp', () => ({
             fare = baseFare * pct / 100;
         }
 
-        return fare > 0 ? fare.toLocaleString() : '-';
+        return fare > 0 ? Alpine.store('currency').format(fare) : '-';
     },
 
     parseFlightDateRange(rangeString) {
@@ -3484,7 +3484,7 @@ Alpine.data('editBookingApp', () => ({
             fare = baseFare * pct / 100;
         }
 
-        return fare > 0 ? fare.toLocaleString() : '-';
+        return fare > 0 ? Alpine.store('currency').format(fare) : '-';
     },
 
     openCustomerModal() {
