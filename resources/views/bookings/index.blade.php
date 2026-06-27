12a06c18f7d226ae76ee56c2fb221c6e13b5de63
FUZLUL HAQUE <fazlul5788@gmail.com>
2026-06-27 15:46:31 +0600

    add logic

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -679,7 +679,11 @@ if ($route) {
             @else
                 <span class="text-slate-300 cursor-not-allowed font-medium">Download</span>
             @endif
-            <a href="{{ route('bookings.download-all-docs', $passenger->booking_id) }}" class="text-green-600 hover:text-green-800 font-medium">Download All</a>
+            @if($passenger->documents_count > 0 || ($passenger->booking->customer?->documents->isNotEmpty() ?? false))
+                <a href="{{ route('bookings.download-all-docs', $passenger->booking_id) }}" class="text-green-600 hover:text-green-800 font-medium">Download All</a>
+            @else
+                <span class="text-slate-300 cursor-not-allowed font-medium">Download All</span>
+            @endif
         </div>
     </td>
 </tr>
