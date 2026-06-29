4d5d9e2a89e2a1e04132c95a2c8c0e0249840e9b
FUZLUL HAQUE <fazlul5788@gmail.com>
2026-06-29 11:23:01 +0600

    Also save the same value to Passenger → Actual Flight

--- app/Observers/IssuedTicketObserver.php
+++ app/Observers/IssuedTicketObserver.php
@@ -11,5 +11,9 @@ class IssuedTicketObserver
         if ($issuedTicket->wasChanged('ticket_fare_id') && $issuedTicket->status === 'issued') {
             $issuedTicket->passenger()->update(['ticket_fare_id' => $issuedTicket->ticket_fare_id]);
         }
+
+        if ($issuedTicket->wasChanged('inbound_date') && $issuedTicket->status === 'issued') {
+            $issuedTicket->passenger()->update(['actual_flight_date' => $issuedTicket->inbound_date]);
+        }
     }
 }
