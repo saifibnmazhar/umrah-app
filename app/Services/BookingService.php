63a0cf6b9370a5a2c9218b28a5db678590b233fc
FUZLUL HAQUE <fazlul5788@gmail.com>
2026-07-02 09:55:54 +0600

    invoice genarate process change

--- app/Services/BookingService.php
+++ app/Services/BookingService.php
@@ -213,7 +213,7 @@ class BookingService
         $prefix = strtoupper($branch?->branch_code ?? '(###)');
 
         do {
-            $random = strtoupper(substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 4));
+            $random = substr(str_shuffle('0123456789'), 0, 4);
             $invoiceId = $prefix . $random . $year;
         } while (Booking::where('invoice_id', $invoiceId)->exists());
 
