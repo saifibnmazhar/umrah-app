9cb0f71c2f2a8cc47b75fbaa6365dc4d50610a37
shantoriaz6 <22103285@iubat.edu>
2026-07-24 18:18:51 +0600

    pegination applied in booking index passenger index and in fingerprint admin

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -3,25 +3,25 @@
         <span class="inline-flex items-center gap-2">
             @if ($paginator->onFirstPage())
                 <span class="px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md cursor-not-allowed leading-5">
-                    {!! __('pagination.previous') !!}
+                    Prev
                 </span>
             @else
                 <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md leading-5 hover:bg-gray-100">
-                    {!! __('pagination.previous') !!}
+                    Prev
                 </a>
             @endif
 
             <span class="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-200 border border-gray-300 rounded-md leading-5">
-                {{ $paginator->currentPage() }}
+                {{ $paginator->currentPage() }}/{{ $paginator->lastPage() }}
             </span>
 
             @if ($paginator->hasMorePages())
                 <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md leading-5 hover:bg-gray-100">
-                    {!! __('pagination.next') !!}
+                    Next
                 </a>
             @else
                 <span class="px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md cursor-not-allowed leading-5">
-                    {!! __('pagination.next') !!}
+                    Next
                 </span>
             @endif
         </span>
