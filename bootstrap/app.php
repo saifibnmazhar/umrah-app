2d3ff60d1b23e63d9f567f4f6a1a036d8b76d560
Azhar Ibn Mostafiz <theazharul@gmail.com>
2026-08-15 00:21:08 +0600

    trust proxies updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,8 +25,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(
             at: '*',
-            headers: Request::HEADER_X_FORWARDED_TRAEFIK
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
         );
+
         $middleware->redirectGuestsTo(fn () => route('login'));
 
         $middleware->alias([
