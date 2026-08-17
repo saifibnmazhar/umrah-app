bd0e4028387328bf53192dbe84b8e630836dd721
shantoriaz6 <22103285@iubat.edu>
2026-08-17 20:44:21 +0600

    refund adjustment fix

--- app/Http/Controllers/TicketRequestController.php
+++ app/Http/Controllers/TicketRequestController.php
@@ -323,6 +323,18 @@ class TicketRequestController extends Controller
                             'notes' => $validated['remarks'] ?? null,
                         ]);
                     }
+
+                    $remainingPayment = $totalCustomerPayment - $amount;
+                    if ($remainingPayment > 0) {
+                        $invoice = $booking->invoice;
+                        if ($invoice) {
+                            app(InvoiceService::class)->updateTotals(
+                                $invoice,
+                                (float) $invoice->total_amount + $remainingPayment,
+                                're_issue_cost_added'
+                            );
+                        }
+                    }
                 } elseif ($totalCustomerPayment > 0) {
                     $invoice = $booking->invoice;
                     if ($invoice) {
