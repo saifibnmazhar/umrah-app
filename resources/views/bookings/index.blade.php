33e93e75e2c02baeb9d4285a5ea05835e46bdfb5
Mostafiz Ur Rahman <cdr.mostafiz@gmail.com>
2026-09-12 11:05:13 +0600

    rename service required filter options

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -1033,9 +1033,9 @@ $passengersTicketData = ($passengers ?? collect())->map(fn($p) => [
                     <div class="flex flex-col">
                         <label class="text-xs font-semibold text-slate-400 mb-1">Service Required</label>
                         <select x-model="selectedServiceRequired" @change="onServiceRequiredChange" class="px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-400 focus:border-slate-400 outline-none transition bg-white text-slate-700">
-                            <option value="all" {{ ($selectedServiceRequired ?? 'all') === 'all' ? 'selected' : '' }}>All</option>
-                            <option value="visa_only" {{ ($selectedServiceRequired ?? '') === 'visa_only' ? 'selected' : '' }}>Visa</option>
-                            <option value="ticket_only" {{ ($selectedServiceRequired ?? '') === 'ticket_only' ? 'selected' : '' }}>Ticket</option>
+                            <option value="all" {{ ($selectedServiceRequired ?? 'all') === 'all' ? 'selected' : '' }}>Visa + Ticket</option>
+                            <option value="visa_only" {{ ($selectedServiceRequired ?? '') === 'visa_only' ? 'selected' : '' }}>Visa Only</option>
+                            <option value="ticket_only" {{ ($selectedServiceRequired ?? '') === 'ticket_only' ? 'selected' : '' }}>Ticket Only</option>
                         </select>
                     </div>
                     <div class="flex flex-col">
